6387aa2a39299fd187a5d57e13bfb16bbd254801
Caner KAYA <cann1936@gmail.com>
2020-03-26 03:31:18 +0300

    Initial commit

---
+++ app/src/main/java/com/canerkaya/professionalartbook/Main2Activity.java
@@ -0,0 +1,135 @@
+package com.canerkaya.professionalartbook;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class Main2Activity extends AppCompatActivity {
+    ImageView imageView;
+    EditText editText;
+    Button saveButton,deleteButton,updateButton;
+    Bitmap selectedImage;
+    String firstName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        imageView=findViewById(R.id.imageView);
+        editText=findViewById(R.id.editText);
+        saveButton=findViewById(R.id.save);
+        deleteButton=findViewById(R.id.delete);
+        updateButton=findViewById(R.id.update);
+
+
+        Intent intent=getIntent();
+        String info=intent.getStringExtra("info");
+        if (info.matches("new")){
+            Bitmap background= BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.background);
+            imageView.setImageBitmap(background);
+            editText.setText("");
+            saveButton.setVisibility(View.VISIBLE);
+            deleteButton.setVisibility(View.INVISIBLE);
+            updateButton.setVisibility(View.INVISIBLE);
+        }else {
+            saveButton.setVisibility(View.INVISIBLE);
+            deleteButton.setVisibility(View.VISIBLE);
+            updateButton.setVisibility(View.VISIBLE);
+            String name=intent.getStringExtra("name");
+            editText.setText(name);
+            firstName=name;
+            int position=intent.getIntExtra("position",0);
+            imageView.setImageBitmap(MainActivity.artImageList.get(position));
+        }
+    }
+    public void saveRecord(View view){
+        String artName=editText.getText().toString();
+        ByteArrayOutputStream outputStream=new ByteArrayOutputStream();
+        selectedImage.compress(Bitmap.CompressFormat.PNG,50,outputStream);
+        byte[]bytes=outputStream.toByteArray();
+        ContentValues contentValues=new ContentValues();
+        contentValues.put(ArtContentProvider.NAME,artName);
+        contentValues.put(ArtContentProvider.IMAGE,bytes);
+        Uri uri=getContentResolver().insert(ArtContentProvider.CONTENT_URI,contentValues);
+        Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+        finish();
+        startActivity(intent);
+    }
+    public void deleteRecord(View view){
+        String recordName=editText.getText().toString();
+        String[] selectionArg={recordName};
+        getContentResolver().delete(ArtContentProvider.CONTENT_URI,"name=?",selectionArg);
+        Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+        finish();
+        startActivity(intent);
+    }
+    public void updateRecord(View view){
+        String artName=editText.getText().toString();
+        ByteArrayOutputStream outputStream=new ByteArrayOutputStream();
+        Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        bitmap.compress(Bitmap.CompressFormat.PNG,50,outputStream);
+        byte[]bytes=outputStream.toByteArray();
+
+        ContentValues contentValues=new ContentValues();
+        contentValues.put(ArtContentProvider.NAME,artName);
+        contentValues.put(ArtContentProvider.IMAGE,bytes);
+        getContentResolver().update(ArtContentProvider.CONTENT_URI,contentValues,"name=?",new String[]{firstName});
+        Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+        finish();
+        startActivity(intent);
+    }
+    public void select(View view){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+        }else {
+            Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent,2);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode==1){
+            if (grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,2);
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode==2 && resultCode==RESULT_OK && data!=null){
+            Uri image=data.getData();
+
+            try {
+                selectedImage= MediaStore.Images.Media.getBitmap(this.getContentResolver(),image);
+                imageView.setImageBitmap(selectedImage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
---
+++ app/src/main/java/com/canerkaya/professionalartbook/MainActivity.java
@@ -0,0 +1,78 @@
+package com.canerkaya.professionalartbook;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    static ArrayList<Bitmap> artImageList;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.add_art,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()==R.id.add_art){
+            Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+            intent.putExtra("info","new");
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView=findViewById(R.id.listView);
+        final ArrayList<String> artNameList=new ArrayList<>();
+        artImageList=new ArrayList<>();
+        ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,artNameList);
+        listView.setAdapter(arrayAdapter);
+        String Url="content://com.canerkaya.professionalartbook.ArtContentProvider";
+        Uri artUri=Uri.parse(Url);
+        ContentResolver contentResolver=getContentResolver();
+        Cursor cursor=contentResolver.query(artUri,null,null,null,null);
+        if (cursor!=null){
+            while (cursor.moveToNext()){
+                artNameList.add(cursor.getString(cursor.getColumnIndex(ArtContentProvider.NAME)));
+                byte[]bytes=cursor.getBlob(cursor.getColumnIndex(ArtContentProvider.IMAGE));
+                Bitmap bitmap= BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+                artImageList.add(bitmap);
+                arrayAdapter.notifyDataSetChanged();
+            }
+        }
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("info","old");
+                intent.putExtra("name",artNameList.get(position));
+                intent.putExtra("position", position);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
